dacc05f0f63fa6364318a1a4baa06fb336138857
anhptncc <anh.phamtien@ncc.asia>
2026-04-10 11:13:35 +0700

    fix: remove dynamic port configuration in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,6 @@ async function bootstrap() {
   const bot = app.get(BotGateway);
   bot.initEvent();
 
-  const port = process.env.PORT || 3000;
-
-  await app.listen(port, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
