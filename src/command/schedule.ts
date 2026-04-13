39ed3b5fba707aff0c6c4ef3174223de7da5bd11
anhptncc <anh.phamtien@ncc.asia>
2026-04-13 15:07:07 +0700

    fix: update command decorator and refactor execute method in InternalScheduleCommand

--- src/command/schedule.ts
+++ src/command/schedule.ts
@@ -3,7 +3,7 @@ import { Command } from '@app/decorators/command.decorator';
 import { CommandMessage } from '@app/command/common/command.abstract';
 import { Ncc8Service } from '@app/services/ncc8.service';
 
-@Command('ncc8schedule', {})
+@Command('ncc8', {})
 export class InternalScheduleCommand extends CommandMessage {
   private readonly NCC8_CHANNEL_ID = process.env.NCC8_CHANNEL_ID;
 
@@ -12,9 +12,16 @@ export class InternalScheduleCommand extends CommandMessage {
   }
 
   execute(args: string[], message: ChannelMessage) {
-    if (message.channel_id === this.NCC8_CHANNEL_ID) {
-      this.ncc8Service.sendWorkSchedule();
-      return;
+    const command = args[0];
+
+    switch (command) {
+      case 'schedule':
+        if (message.channel_id === this.NCC8_CHANNEL_ID) {
+          this.ncc8Service.sendWorkSchedule();
+        }
+        break;
+      default:
+        break;
     }
   }
 }
